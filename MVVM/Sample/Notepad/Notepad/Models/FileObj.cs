b02307fb78ea89ff9ab37de35b140ec02aadd35b
Michal Moudrý <moudry.michal@live.com>
2017-04-09 18:33:20 +0200

    Add files via upload

---
+++ MVVM/Sample/Notepad/Notepad/Models/FileObj.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Models
+{
+    public class FileObj
+    {
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+
+        private string _data;
+        public string Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+    }
+}
---
+++ MVVM/Sample/Notepad/Notepad/ViewModels/FileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notepad.Models;
+using Notepad.Services;
+
+namespace Notepad.ViewModels
+{
+    public class FileViewModel
+    {
+        public void SaveFile(FileObj file)
+        {
+            FileService fs = new FileService();
+            fs.Save(file.Data, "File", "json");
+        }
+
+        public FileObj LoadFile()
+        {
+            FileService fs = new FileService();
+            FileObj file = new FileObj();
+            file.Data = fs.Load("File", "json");
+            return file;
+        }
+    }
+}
