f82a469448cc7fb74b4aa23fd46295f08dd1ea56
Michal Moudrý <moudry.michal@live.com>
2017-04-09 18:36:59 +0200

    Branch merge
    Branch merge

---
+++ MVVM/Sample/Notepad/Notepad/Services/FileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Notepad.Models;
+
+namespace Notepad.Services
+{
+    public class FileService
+    {
+        public void Save(string data, string fileName, string fileType)
+        {
+            string path = $"{Directory.GetCurrentDirectory()}\\{fileName}.{fileType}";
+            File.Delete(path);
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.Write(data);
+            }
+        }
+
+        public string Load(string fileName, string fileType)
+        {
+            string path = $"{Directory.GetCurrentDirectory()}\\{fileName}.{fileType}";
+            string text;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+            return text;
+        }
+
+    }
+}
---
+++ MVVM/Sample/Notepad/Notepad/Views/RootPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Notepad.ViewModels;
+using Notepad.Models;
+
+namespace Notepad.Views
+{
+    /// <summary>
+    /// Interakční logika pro RootPage.xaml
+    /// </summary>
+    public partial class RootPage : Page
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+            fsv = new FileViewModel();
+            LoadData();
+        }
+
+        FileViewModel fsv;
+        FileObj file;
+
+        private void saveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            file = new FileObj();
+            file.Data = new TextRange(notepad.Document.ContentStart, notepad.Document.ContentEnd).Text;
+            DisableUIElements();
+            fsv.SaveFile(file);
+            EnableUIElements();
+        }
+        private void loadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        /// <summary>
+        /// Method for disabling elements in UI.
+        /// </summary>
+        private void DisableUIElements()
+        {
+            saveFileButton.IsEnabled = false;
+            notepad.IsEnabled = false;
+        }
+        /// <summary>
+        /// Method for enabling elements in UI.
+        /// </summary>
+        private void EnableUIElements()
+        {
+            saveFileButton.IsEnabled = true;
+            notepad.IsEnabled = true;
+        }
+        /// <summary>
+        /// Method for setting data in RichTextBox control.
+        /// </summary>
+        private void LoadData()
+        {
+            TextRange tr = new TextRange(notepad.Document.ContentStart, notepad.Document.ContentEnd);
+            tr.Text = fsv.LoadFile().Data;
+        }
+        
+    }
+}
